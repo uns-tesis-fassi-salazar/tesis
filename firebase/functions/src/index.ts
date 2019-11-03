e45be46189ee132cc7f80e061d68bd18092e82aa
Jere <jeremiasfassi@gmail.com>
2019-11-03 17:24:40 -0300

    MovSensor + Firebase Test

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,5 +1,3 @@
-import * as functions from 'firebase-functions';
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
@@ -7,8 +5,21 @@ import * as functions from 'firebase-functions';
 //  response.send("Hello from Firebase!");
 // });
 
+import * as functions from 'firebase-functions';
+import * as telegraf from 'telegraf';
 import * as express from 'express';
 import * as cors from 'cors';
+
+export const notification = functions.database.instance('finalproject-35a1b').ref('/MovementSensor/value').onUpdate(_ => {
+  // const bot = new telegraf.Telegram(functions.config().bot.token);
+  const bot = new telegraf.Telegram("980897224:AAGnION1Ii8m0H3du-BC9-4KoRPkW--FcbY");
+  return bot.sendMessage(
+      // functions.config().bot.chat,
+      "-336904340",
+      'Cambio el value de MovementSensor en firebase. wiiii'
+  );
+});
+
 
 // give us the possibility of manage request properly
 const app = express()
@@ -32,12 +43,15 @@ app.post('/', async (req, res) => {
 
   if (isTelegramMessage) {
     const chat_id = req.body.message.chat.id
-    const { first_name } = req.body.message.from
+    // const { first_name } = req.body.message.from
+
+    const reqbody = JSON.stringify(req.body.message)
 
     return res.status(200).send({
       method: 'sendMessage',
       chat_id,
-      text: `Hello ${first_name}`
+      // text: `Hello ${first_name}`
+      text: reqbody
     })
   }
 
