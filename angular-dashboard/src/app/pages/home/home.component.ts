c41c095a0e2229bc6baf66ee456d11119b07225c
gsalazar <gsalazar@teladochealth.com>
2021-01-18 07:34:28 -0300

    update and add images of the project

--- angular-dashboard/src/app/pages/home/home.component.ts
+++ angular-dashboard/src/app/pages/home/home.component.ts
@@ -16,11 +16,15 @@ export class HomeComponent implements OnInit {
       '1.jpeg',
       '2.jpeg',
       '3.jpeg',
-      '4.jpeg',
+      '4.jpg',
       '5.jpeg',
       '6.jpeg',
       '7.jpeg',
       '8.jpeg',
+      '9.jpg',
+      '10.jpg',
+      // '11.jpg',
+      // '12.jpg',
     ];
   }
 
